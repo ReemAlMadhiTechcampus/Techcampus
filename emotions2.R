0b10bcbafae8231ceec9d776effb8098b2d743b8
ReemAlMadhiTechcampus <48881284+ReemAlMadhiTechcampus@users.noreply.github.com>
2019-03-25 11:50:00 +0300

    r file

---
+++ emotions2.R
@@ -0,0 +1,12 @@
+
+#  Install Requried Packages
+installed.packages("SnowballC")
+installed.packages("tm")
+installed.packages("twitteR")
+installed.packages("syuzhet")
+
+# Load Requried Packages
+library("SnowballC")
+library("tm")
+library("twitteR")
+library("syuzhet")
